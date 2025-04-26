e8a87e588a501e1b9cc7832afe80e1dcfebfd967
Karan <karanpradhan96@gmail.com>
2025-04-26 19:38:16 -0400

    attempt

--- sys/project_sys.c
+++ sys/project_sys.c
@@ -30,7 +30,6 @@
 
 #define MAX_PRESSES 100
 
-MODULE_LICENSE("GPL");
 
 static struct hrtimer led1_timer, led2_timer, led3_timer, btn_poll_timer;
 static struct kobject *project_kobj;
@@ -41,6 +40,10 @@ static int last_btn1 = 1, last_btn2 = 1;
 static int last_button_pressed = 0;
 static int speed = 0;
 
+static ktime_t led1_on, led1_off, led2_on, led2_off, led3_on, led3_off;
+static bool led1_state, led2_state, led3_state;
+static char read_buf[BUF_LEN + 1];
+
 typedef struct {
     uint32_t timestamp;
     int button_id;
@@ -49,38 +52,49 @@ typedef struct {
 static button_event_t press_events[MAX_PRESSES];
 static int press_idx = 0;
 
-static void set_led(int led_gpio, bool state)
+static void init_led_gpios(void)
 {
-    if (state)
-        writel(1 << led_gpio, addr + (GPSET_OFFSET / 4));
-    else
-        writel(1 << led_gpio, addr + (GPCLR_OFFSET / 4));
+    addr = ioremap(GPIO_BASE_ADDR, 4*16);
+    if (!addr)
+        return;
+    // GPFSEL0: GPIO2 output (bits 6-8 = 001)
+    writel((1 << 6), addr);
+    // GPFSEL1: GPIO17 output (bits 21-23 = 001)
+    // GPFSEL1: GPIO12 output (bits 6-8 = 001)
+    writel(((1 << 21)|(1 << 6)), addr+1);
 }
 
-static enum hrtimer_restart led_timer_cb(struct hrtimer *timer)
+static enum hrtimer_restart led1_cb(struct hrtimer *timer)
 {
-    if (timer == &led1_timer) {
-        static bool on1 = false;
-        on1 = !on1;
-        set_led(GPIO_LED1, on1);
-        hrtimer_forward_now(timer, ktime_set(0, led1_duty * 1000));
-    } else if (timer == &led2_timer) {
-        static bool on2 = false;
-        on2 = !on2;
-        set_led(GPIO_LED2, on2);
-        hrtimer_forward_now(timer, ktime_set(0, led2_duty * 1000));
-    } else if (timer == &led3_timer) {
-        static bool on3 = false;
-        on3 = !on3;
-        set_led(GPIO_LED3, on3);
-        hrtimer_forward_now(timer, ktime_set(0, led3_duty * 1000));
-    }
+    led1_state = !led1_state;
+    writel((1<<GPIO_LED1), led1_state ? (addr+7) : (addr+10));
+    hrtimer_forward_now(&led1_timer, led1_state ? led1_on : led1_off);
+    
+    return HRTIMER_RESTART;
+}
+
+static enum hrtimer_restart led2_cb(struct hrtimer *timer)
+{
+    led2_state = !led2_state;
+    writel((1<<GPIO_LED2), led2_state ? (addr+7) : (addr+10));
+    hrtimer_forward_now(&led2_timer, led2_state ? led2_on : led2_off);
+
+    return HRTIMER_RESTART;
+}
+
+static enum hrtimer_restart led3_cb(struct hrtimer *timer)
+{
+    led3_state = !led3_state;
+    writel((1<<GPIO_LED3), led3_state ? (addr+7) : (addr+10));
+    hrtimer_forward_now(&led3_timer, led3_state ? led3_on : led3_off);
+
     return HRTIMER_RESTART;
 }
 
 static void record_press(int button_id)
 {
     uint32_t now_sec = (uint32_t)(ktime_get_real_seconds());
+
     if (press_idx < MAX_PRESSES) {
         press_events[press_idx].timestamp = now_sec;
         press_events[press_idx].button_id = button_id;
@@ -101,6 +115,7 @@ static void calculate_speed(void)
     int i;
     int last_button = 0;
 
+    // Purge old events
     int start_idx = press_idx;
     for (i = 0; i < press_idx; i++) {
         if (now_sec - press_events[i].timestamp <= 10) {
@@ -111,13 +126,15 @@ static void calculate_speed(void)
 
     if (start_idx != 0 && start_idx < press_idx) {
         int new_idx = 0;
-        for (i = start_idx; i < press_idx; i++)
+        for (i = start_idx; i < press_idx; i++) {
             press_events[new_idx++] = press_events[i];
+        }
         press_idx = new_idx;
-    } else if (start_idx == press_idx) {
-        press_idx = 0;
     }
+    else if (start_idx == press_idx)
+        press_idx = 0;
 
+    // Count valid alternating presses
     for (i = 0; i < press_idx; i++) {
         if (press_events[i].button_id != last_button) {
             count++;
@@ -126,6 +143,7 @@ static void calculate_speed(void)
     }
 
     speed = count;
+    sprintf(read_buf, "%d\n", speed);
 }
 
 static enum hrtimer_restart btn_poll_cb(struct hrtimer *timer)
@@ -134,15 +152,17 @@ static enum hrtimer_restart btn_poll_cb(struct hrtimer *timer)
     int btn1 = (gplev >> GPIO_BTN1) & 1;
     int btn2 = (gplev >> GPIO_BTN2) & 1;
 
-    if (last_btn1 == 1 && btn1 == 0) {
-        if (last_button_pressed != 1) {
+    if (last_btn1 == 1 && btn1 == 0) { // BTN1 falling edge
+        pr_info("BTN1 pressed\n");
+        if (last_button_pressed != 1) {  // Only if last was not BTN1
             record_press(1);
             last_button_pressed = 1;
         }
     }
 
-    if (last_btn2 == 1 && btn2 == 0) {
-        if (last_button_pressed != 2) {
+    if (last_btn2 == 1 && btn2 == 0) { // BTN2 falling edge
+        pr_info("BTN2 pressed\n");
+        if (last_button_pressed != 2) {  // Only if last was not BTN2
             record_press(2);
             last_button_pressed = 2;
         }
@@ -151,7 +171,7 @@ static enum hrtimer_restart btn_poll_cb(struct hrtimer *timer)
     last_btn1 = btn1;
     last_btn2 = btn2;
 
-    hrtimer_forward_now(timer, ktime_set(0, 1000000));
+    hrtimer_forward_now(timer, ktime_set(0, 1000000)); // 1ms
     return HRTIMER_RESTART;
 }
 
@@ -168,11 +188,26 @@ static struct kobj_attribute speed_attr = __ATTR(speed, 0660, speed_show, NULL);
 
 static ssize_t led1_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
-    sscanf(buf, "%d", &led1_duty);
-    pr_info("LED1 duty updated to %d\n", led1_duty);
+    int duty;
+    ktime_t on, off;
+
+    if (sscanf(buf, "%d", &duty) != 1) {
+        pr_info("device_write: bad format '%s'\n", buf);
+        return -EINVAL;
+    }
+
+    switch (duty) {
+        case 0:   pr_info("got duty 0\n"); on = ktime_set(0, TIME_0);   off = ktime_set(0, TIME_100); break;
+        case 25:  pr_info("got duty 25\n"); on = ktime_set(0, TIME_25);  off = ktime_set(0, TIME_75); break;
+        case 50:  pr_info("got duty 50\n"); on = ktime_set(0, TIME_50);  off = ktime_set(0, TIME_50); break;
+        case 75:  pr_info("got duty 75\n"); on = ktime_set(0, TIME_75);  off = ktime_set(0, TIME_25); break;
+        case 100: pr_info("got duty 100\n"); on = ktime_set(0, TIME_100); off = ktime_set(0,   TIME_0); break;
+        default:  pr_info("device_write: invalid duty '%d'; only supports 0, 25, 50, 75, 100!\n", duty); return -EINVAL;
+    }
 
     hrtimer_cancel(&led1_timer);
-    hrtimer_start(&led1_timer, ktime_set(0, led1_duty * 1000), HRTIMER_MODE_REL);
+    led1_state=false;
+    hrtimer_start(&led1_timer, ktime_set(0,0), HRTIMER_MODE_REL);
 
     return count;
 }
@@ -181,11 +216,7 @@ static struct kobj_attribute led1_attr = __ATTR(led1, 0660, NULL, led1_store);
 
 static ssize_t led2_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
-    sscanf(buf, "%d", &led2_duty);
-    pr_info("LED2 duty updated to %d\n", led2_duty);
-
-    hrtimer_cancel(&led2_timer);
-    hrtimer_start(&led2_timer, ktime_set(0, led2_duty * 1000), HRTIMER_MODE_REL);
+    
 
     return count;
 }
@@ -194,11 +225,7 @@ static struct kobj_attribute led2_attr = __ATTR(led2, 0660, NULL, led2_store);
 
 static ssize_t led3_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
-    sscanf(buf, "%d", &led3_duty);
-    pr_info("LED3 duty updated to %d\n", led3_duty);
-
-    hrtimer_cancel(&led3_timer);
-    hrtimer_start(&led3_timer, ktime_set(0, led3_duty * 1000), HRTIMER_MODE_REL);
+    
 
     return count;
 }
@@ -224,7 +251,7 @@ static int __init project_init(void)
     int retval;
 
     pr_info("project_sys: Module initialized\n");
-    addr = ioremap(GPIO_BASE_ADDR, 0x100);
+    addr = ioremap(GPIO_BASE_ADDR, 4*16);
 
     project_kobj = kobject_create_and_add(DEVICE_NAME, kernel_kobj);
     if (!project_kobj)
@@ -237,11 +264,11 @@ static int __init project_init(void)
     hrtimer_init(&led1_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
     hrtimer_init(&led2_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
     hrtimer_init(&led3_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
-    hrtimer_init(&btn_poll_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+    led1_timer.function = &led1_cb;
+    led2_timer.function = &led2_cb;
+    led3_timer.function = &led3_cb;
 
-    led1_timer.function = led_timer_cb;
-    led2_timer.function = led_timer_cb;
-    led3_timer.function = led_timer_cb;
+    hrtimer_init(&btn_poll_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
     btn_poll_timer.function = btn_poll_cb;
 
     hrtimer_start(&btn_poll_timer, ktime_set(0, 1000000), HRTIMER_MODE_REL);
@@ -263,3 +290,5 @@ static void __exit project_exit(void)
 
 module_init(project_init);
 module_exit(project_exit);
+
+MODULE_LICENSE("GPL");
